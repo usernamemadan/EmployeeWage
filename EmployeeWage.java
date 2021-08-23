f4559dbead9b76aeda67e72bbbe439506d256f2f
Madan AR <madanar@106AMBBLR-Nithin-A-Y.local>
2021-08-23 22:20:12 +0530

    added main method to display welcome message

---
+++ EmployeeWage.java
@@ -0,0 +1,12 @@
+package com.employee;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to employee wage computation problem");
+		
+	}
+}
+
+
