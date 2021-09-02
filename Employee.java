5045e47b4b98d813041157fe94b616899e1d8470
madan <madan@local.com>
2021-09-02 20:21:43 +0530

    modified totolEmployeeWage method to take arguments for different employees

--- Employee.java
+++ Employee.java
@@ -4,24 +4,13 @@ public class Employee {
 		
 		final static int IS_FULL_TIME = 2;
 		final static int IS_PART_TIME = 1;
-		final static int NUM_WORKING_DAYS = 20;
-		final static int EMP_RATE_PER_HOUR = 20;
-		final static int MAX_HRS_IN_MONTH = 100;
-		
-		static int empHrs = 0;
-		static int empWage = 0;
-		static int totalEmpWage = 0;
-		static int totalEmpHrs = 0;
-		static int totalWorkingDays = 0;
-		static int empCheck = 0;
 		
 		Employee(){
 			System.out.println("Welcome to employee wage computation problem");
-			empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		}
 		
-		public void isEmployeePresent() {
-			empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		public static void isEmployeePresent() {
+			 int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch ( empCheck) {
 				case IS_PART_TIME:
 					System.out.println("Employee is working part time");
@@ -34,26 +23,15 @@ public class Employee {
 			}
 		}
 		
-		public static void employeeWage() {
-			switch ( empCheck) {
-			case IS_PART_TIME:
-				empHrs = 4;
-				break;
-			case IS_FULL_TIME:
-				empHrs =8;
-				break;
-			default:
-				empHrs = 0;
+		
+		public static void totalEmployeeWage(int wagePerHour, int workingHoursPerMonth, int workingDaysPerMonth) {
+			 int empHrs = 0;
+			 int totalEmpWage = 0;
+			 int totalEmpHrs = 0;
+			 int totalWorkingDays = 0;
 			
-			}
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			System.out.println("employee wage is " + empWage);
-		}
-		
-		
-		public static void totalEmployeeWage() {
-			while ( totalEmpHrs <= MAX_HRS_IN_MONTH 
-					  && totalWorkingDays < NUM_WORKING_DAYS) {
+			while ( totalEmpHrs <= workingHoursPerMonth 
+					  && totalWorkingDays < workingDaysPerMonth) {
 				
 				int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 				switch ( empCheck) {
@@ -71,7 +49,7 @@ public class Employee {
 				System.out.println("Day -" + totalWorkingDays + " emp hours " + empHrs);
 				
 			}
-			totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+			totalEmpWage = totalEmpHrs * wagePerHour;
 			System.out.println("total wage of the employee: "+ totalEmpWage);
 		}
 		
--- Main.java
+++ Main.java
@@ -2,9 +2,12 @@ package com.employee;
 
 public class Main {
 	public static void main(String[] args) {
-		Employee employee = new Employee();
-		employee.isEmployeePresent();
-		employee.employeeWage();
-		employee.totalEmployeeWage();
+		Employee employee1 = new Employee();
+		employee1.isEmployeePresent();
+		employee1.totalEmployeeWage(40, 35 , 30);
+		
+		Employee employee2 = new Employee();
+		employee2.isEmployeePresent();
+		employee2.totalEmployeeWage(79, 40 , 23);
 	}
 }
